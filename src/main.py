d67a623305e502318da4e22b97f06eb3a4ba51fe
Elon Goliger <egoliger@exploratorium.edu>
2023-05-10 22:28:19 -0700

    add content to main file

---
+++ src/main.py
@@ -0,0 +1,74 @@
+#dependencies
+from pint import UnitRegistry
+import numpy as np
+import pandas as pd
+
+#unit setup
+ureg = UnitRegistry()
+m = ureg.meter
+g = ureg.gram
+kg = ureg.kilogram
+s = ureg.second
+min = ureg.minute
+hr = ureg.hour
+rpm = ureg.revolution / min
+turns = ureg.turn
+kPa = ureg.kilopascal
+W = ureg.watt
+N = ureg.newton
+K = ureg.kelvin
+#variable array setup
+speed = ureg.Quantity(np.array([1500, 2000, 2500, 3000, 3500, 4000]), rpm)
+torque = ureg.Quantity(np.array([1.8, 1.8, 1.8, 1.8, 1.8, 1.8]), N * m)
+Qdot_shaft = ureg.Quantity(np.array([None, None, None, None, None, None]), W) # Qdot_shaft
+Qdot_in = ureg.Quantity(np.array([None, None, None, None, None, None]), W) # heat input
+efficiency = np.array([None, None, None, None, None, None]) # n_th percent
+MEP = ureg.Quantity(np.array([None, None, None, None, None, None]), kPa) # mean effective pressure
+bsfc = ureg.Quantity(np.array([None, None, None, None, None, None]), g / (W * hr)) # brake specific fuel consumption
+Qdot_exhaust = ureg.Quantity(np.array([None, None, None, None, None, None]), W) # exhaust heat
+Qdot_fins = ureg.Quantity(np.array([None, None, None, None, None, None]), W) # fins heat
+mdot_air = ureg.Quantity(np.array([None, None, None, None, None, None]), kg/s) # mass flow rate of air
+mdot_fuel = ureg.Quantity(np.array([5.55E-05, 5.64E-05, 6.83E-05, 8.26E-05, 0.000104, 0.000118]), kg / s) # mass flow rate of fuel
+deltaT = ureg.Quantity(np.array([247, 247, 247, 291, 330, 350]), K) # temperature difference
+
+#constants
+rho_air = 1.2 * ureg.kilogram / ureg.meter**3
+Cp_air = 1.006 * ureg.kilojoule / (ureg.kilogram * ureg.kelvin)
+LHV_gas = 45.2 * ureg.kilojoule / ureg.gram
+#calculations
+D = 10e-4 * m**3 # displacement of the engine
+mdot_air = (rho_air * D * speed) / (2*turns) # The 2 is present in the denominator because engine will only draw air every second revolution
+mdot_air = mdot_air.to(kg / s)
+Qdot_exhaust = mdot_air * Cp_air * deltaT
+Qdot_in = mdot_fuel * LHV_gas
+Qdot_in = Qdot_in.to(W)
+Qdot_shaft = speed*torque
+Qdot_shaft = Qdot_shaft.to(W)
+Qdot_fins = Qdot_in - Qdot_exhaust - Qdot_shaft
+efficiency = (Qdot_shaft / Qdot_in)*100 # n_th percent
+bsfc = mdot_fuel / Qdot_shaft
+bsfs = bsfc.to(g / (W * hr))
+MEP = 4 * np.pi * (torque / D)
+MEP = MEP.to(kPa)
+
+# Create a dictionary with the column names and data
+data = {
+    'Speed (RPM)': speed.magnitude,
+    'Torque (N m)': torque.magnitude,
+    'Power Shaft (W)': Qdot_shaft.magnitude,
+    'Power Input (W)': Qdot_in.magnitude,
+    'Efficiency (%)': efficiency.magnitude,
+    'MEP (kPa)': MEP.magnitude,
+    'BSFC (g/(W*h))': bsfc.magnitude,
+    'Exhaust Heat (W)': Qdot_exhaust.magnitude,
+    'Fins Heat (W)': Qdot_fins.magnitude,
+    'Air Mass Flow (g/s)': mdot_air.magnitude,
+    'Fuel Mass Flow (g/s)': mdot_fuel.magnitude,
+    'Delta T (delta_K)': deltaT.magnitude
+}
+
+# Create a DataFrame from the dictionary
+df = pd.DataFrame(data)
+
+# Display the DataFrame in a nice table
+df
